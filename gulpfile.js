618d9a85f06ab2b0ca3dbe95d92b93171e24197e
Ian Williams <ianwilliams633@gmail.com>
2016-05-23 18:34:23 -0700

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,88 @@
+var gulp       = require('gulp'),
+    browserify = require('browserify'),
+    babelify   = require('babelify'),
+    concat     = require('gulp-concat'),
+    buffer     = require('vinyl-buffer'),
+    uglify     = require('gulp-uglify'),
+    source     = require('vinyl-source-stream');
+
+var defaultTasks = [];
+var buildTasks   = [];
+var dest         = './dist';
+var jsSource     = './src/js';
+var jsEntry      = jsSource += '/progressive.js';
+var jsOutput     = 'progressive.js';
+
+var bowerDir = function(dir) {
+    return './bower_components/' + dir;
+};
+
+var includes = {
+    foundation: bowerDir('foundation/scss')
+};
+
+// Helper function to transpile JS
+// http://stackoverflow.com/questions/24992980/how-to-uglify-output-with-browserify-in-gulp
+function transpileJS(entryPoint, outputName, outputPath, debug) {
+    return function() {
+        var bundler = browserify(entryPoint, { debug: debug, standalone: 'Progressive' })
+          .transform(babelify)
+          .bundle()
+          .on("error", function (err) { console.log("Error : " + err.message); })
+          .pipe(source(outputName));
+
+        if(!debug) {
+            bundler = bundler.pipe(buffer())
+              .pipe(uglify());
+        }
+
+        return bundler.pipe(gulp.dest(outputPath));
+    }
+}
+
+// Compile client es6 code
+gulp.task('client-code', transpileJS(jsEntry, jsOutput, dest + '/js', true));
+gulp.task('prod-code', transpileJS(jsEntry, jsOutput, dest + '/js', false));
+defaultTasks.push('client-code');
+buildTasks.push('prod-code');
+
+// Vendor Scripts
+gulp.task('vendor-scripts', function() {
+    return gulp.src([
+        //includes.three + 'three.min.js',
+    ])
+    .pipe(concat('vendor.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(dest + '/js'));
+});
+defaultTasks.push('vendor-scripts');
+buildTasks.push('vendor-scripts');
+
+// Watches script directories and rebuilds on change
+gulp.task('watch-scripts', function() {
+    gulp.watch([
+        jsSource + '/**/*.js',
+    ], function() {
+        console.log("~~~ Rebuilding Scripts... ~~~");
+    });
+
+    gulp.watch([
+        './src/js/**/*.js'
+    ], ['client-code']);
+});
+defaultTasks.push('watch-scripts');
+
+gulp.task('move-resources', function() {
+    gulp.src('resources/assets/**/*')
+        .pipe(gulp.dest('./html/assets'));
+});
+defaultTasks.push('move-resources');
+buildTasks.push('move-resources');
+
+gulp.task('watch-resources', function() {
+    gulp.watch(['./resources/assets/**/*'], ['move-resources']);
+});
+defaultTasks.push('watch-resources');
+
+gulp.task('default', defaultTasks);
+gulp.task('build', buildTasks);
---
+++ src/js/progressive.js
@@ -0,0 +1,156 @@
+export default class Progressive {
+
+    /**
+     * sources:
+     * [
+     *      // Sources to load first
+     *      [
+     *          { src: '....', type: '....' },
+     *          { src: '....', type: '....' },
+     *          { src: '....', type: '....' },
+     *      ],
+     *      // Sources to load second
+     *      [
+     *          { src: '....', type: '....' },
+     *          { src: '....', type: '....' },
+     *          { src: '....', type: '....' },
+     *      ],
+     *      // Add as many as you want?
+     *      [
+     *          ....
+     *      ]
+     * ]
+     *
+     **/
+    constructor(videoContainer, videoSources) {
+        let container   = this._container = videoContainer.nodeType ? videoContainer : document.querySelector(videoContainer);
+        let videos      = this._videos    = videoSources;
+
+        // Create canvas element
+        let canvas = this._canvas = document.createElement('canvas');
+        canvas.width = container.clientWidth;
+        canvas.height = container.clientHeight;
+        container.appendChild(canvas);
+
+        // Save ctx
+        let ctx = this._ctx = canvas.getContext('2d');
+
+        // Setup video elements
+        let videoElements = this._videoElements = videos.map((sources, idx, arr) => {
+            let videoElement = document.createElement('video');
+            videoElement.setAttribute('preload', 'none');
+            videoElement.setAttribute('loop',    'true');
+
+            sources.forEach(source => {
+                let sourceElement = document.createElement('source');
+                sourceElement.setAttribute('src',  source.src);
+                sourceElement.setAttribute('type', source.type);
+                videoElement.appendChild(sourceElement);
+            });
+
+            if(idx === 0) {
+                let loaded = false;
+                let canPlayThrough = e => {
+                    this._setActiveVideo(videoElement);
+                    this._render();
+                    videoElements[1].load();
+                    videoElement.removeEventListener('canplaythrough', canPlayThrough);
+                }
+                videoElement.addEventListener('canplaythrough', canPlayThrough);
+            } else {
+                let canPlayThrough = e => {
+                    this._setActiveVideo(videoElement);
+                    videoElement.removeEventListener('canplaythrough', canPlayThrough);
+                }
+                videoElement.addEventListener('canplaythrough', canPlayThrough);
+            }
+
+            return videoElement;
+        });
+
+        window.addEventListener('resize', this._calculateSize.bind(this));
+
+        videoElements[0].load();
+    }
+
+
+
+
+    /**
+     * Set active video
+     */
+    _setActiveVideo(video) {
+        function swap() {
+            this._activeVideo = video;
+            this._calculateSize();
+            video.play();
+        }
+
+        if(!this._activeVideo) {
+            swap.call(this);
+        } else {
+            this._activeVideo.addEventListener('seeked', e => {
+                this._activeVideo.pause();
+                swap.call(this);
+            });
+        }
+    }
+
+
+
+
+    /**
+     * Loop draw function
+     */
+    _render() {
+        this._draw();
+        this._animationKey = window.requestAnimationFrame(this._render.bind(this));
+    }
+
+
+
+
+    /**
+     * Draw current video to canvas
+     */
+    _draw() {
+        this._ctx.drawImage(this._activeVideo, this._videoX, this._videoY, this._videoWidth, this._videoHeight);
+    }
+
+
+
+
+    /**
+     * Calculate and size the video/canvas
+     */
+    _calculateSize() {
+        this._canvas.width  = this._container.clientWidth;
+        this._canvas.height = this._container.clientHeight;
+
+        let videoW  = this._activeVideo.videoWidth
+          , videoH  = this._activeVideo.videoHeight
+          , minW    = this._canvas.width
+          , minH    = this._canvas.height
+
+        // Intrinsic ratio
+        // Will be more than 1 if W > H and less if H > W
+        var videoRatio = (videoW / videoH).toFixed(2);
+
+        var widthRatio = minW / videoW;
+        var heightRatio = minH / videoH;
+
+        // Whichever ratio is more, the scaling
+        // has to be done over that dimension
+        if (widthRatio > heightRatio) {
+            this._videoWidth  = minW;
+            this._videoHeight = Math.ceil( this._videoWidth / videoRatio );
+        }
+        else {
+            this._videoHeight = minH;
+            this._videoWidth  = Math.ceil( this._videoHeight * videoRatio );
+        }
+
+        this._videoX = (minW - this._videoWidth)/2;
+        this._videoY = (minH - this._videoHeight)/2;
+    }
+}
